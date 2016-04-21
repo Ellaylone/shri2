604bb8d615760729e86b430754049f18675cb704
Ellaylone <heamikallove@gmail.com>
2016-04-21 23:31:41 +0000

    json path change, fixed json

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.normalize(__dirname + '/../client/index.html'));
 });
 
-app.get('/getStudents', function (req, res) {
+app.get('/api/getStudents', function (req, res) {
     res.setHeader('Content-Type', 'application/json; charset=UTF-8');
     res.sendFile(path.normalize(__dirname + '/data/students.json'));
 });
